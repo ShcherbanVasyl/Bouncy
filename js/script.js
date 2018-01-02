e3ac16461fdecf05cef5f1268b6f067ea997231e
ShcherbanVasyl <shcherban050793@gmail.com>
2018-01-02 12:43:48 +0200

    Add About Us Details

--- js/script.js
+++ js/script.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
   $('.toggler-icon').on('click', function() {
     $('.nav-bar').slideToggle('0.5s');
   });
+
+  $(".icon:nth-child(1)").addClass("icon-on");
+  $(".content:nth-child(1)").addClass("show");
+
+  $(".icon").click(function(){
+    $(".icon").removeClass("icon-on");
+    $(this).addClass("icon-on");
+    var index = $(this).index()+1;
+    $(".content").removeClass("show");
+    $(".content:nth-child("+ index +")").addClass("show");
+  });
 });
